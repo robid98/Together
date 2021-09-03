a91842dcbcf795ec70378b88718e2aa49f8cfe0f
Robid98 <robertdarabana796@gmail.com>
2021-09-03 23:08:16 +0300

    New Class Library Project for Data Models, Repositories and Interfaces

---
+++ Backend.Together/Together.Data/Models/PostModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Together.Data.Models
+{
+    public class PostModel
+    {
+        public Guid PostId { get; set; }
+
+        public string PostDescription { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public int PostLikes { get; set; }
+
+        public int PostShares { get; set; }
+
+    }
+}
